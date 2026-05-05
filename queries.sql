23442e00de59cec55abf9dd4ac56960e62fdad7a
Elodie S <elodie.sevestre86@gmail.com>
2026-05-05 15:41:13 +0200

    init queries

--- queries.sql
+++ queries.sql
@@ -1,1 +1,13 @@
 -- queries.sql contient les requêtes SELECT pour récupérer certaines données de la base
+
+-- Dernière étape, récupérer les données. Écris les requêtes SQL de sélection suivantes :
+-- [ ] Récupérer la liste des thèmes
+-- [ ] Récupérer toutes les ressources triées par date de mise à jour
+-- [ ] Récupérer uniquement le nom et l'url des ressources de type exercice
+-- [ ] Récupérer le titre et la description des ressources d'Ada uniquement
+-- [ ] Récupérer les ressources qui ont la compétence JavaScript associée
+-- [ ] Récupérer les ressources dont le titre contient le mot "react" (avec ou sans majuscules)
+
+SELECT name FROM themes;
+
+SELECT * FROM resources ORDER BY ;
