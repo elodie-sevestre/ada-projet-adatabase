f40b26454930d94b17bbc52504c598f32b34633f
Elodie S <elodie.sevestre86@gmail.com>
2026-05-06 15:35:06 +0200

    projet done

--- queries.sql
+++ queries.sql
@@ -9,10 +9,10 @@ SELECT themes.name FROM themes;
 SELECT * FROM resources ORDER BY updated_at DESC;
 
 -- [ ] Récupérer uniquement le nom et l'url des ressources de type exercice
-SELECT title, url FROM resources WHERE type = 'exercice';
+SELECT resources.title, resources.url FROM resources WHERE type = 'exercice';
 
 -- [ ] Récupérer le titre et la description des ressources d'Ada uniquement
-SELECT title, description FROM resources WHERE is_ada = TRUE;
+SELECT resources.title FROM resources WHERE is_ada = TRUE;
 
 -- [ ] Récupérer les ressources qui ont la compétence JavaScript associée
 -- JOIN = INNER JOIN sous entendu
@@ -29,3 +29,30 @@ SELECT resources.title FROM resources WHERE title ILIKE '%react%';
 SELECT themes.name, COUNT(resources.id) AS nb_of_resources FROM themes
 JOIN resources ON resources.theme_id = themes.id
 GROUP BY themes.name;
+
+-- [ ] Récupérer le nom et l'url de toutes les ressources avec un tableau/liste contenant l'ensemble de leurs skills associés
+
+SELECT resources.title, resources.url, STRING_AGG(skills.name, ', ') AS skills FROM resources
+JOIN resources_skills ON resources.id = resources_skills.resource_id
+JOIN skills ON resources_skills.skill_id = skills.id
+GROUP BY resources.title, resources.url;
+
+-- [ ] Récupérer les 5 ressources les plus récentes avec leur thème (ici 3 car je n'ai que 4 ressources et màj d'un statut pour avoir une date différente)
+
+UPDATE resources SET is_ada = TRUE, updated_at = NOW() WHERE resources.id = 3;
+
+SELECT resources.updated_at, resources.title, themes.name FROM resources
+JOIN themes ON themes.id = resources.theme_id
+ORDER BY resources.updated_at DESC
+LIMIT 3;
+
+-- [ ] Récupérer toutes les compétences qui ne sont associées à aucune ressource
+
+INSERT INTO skills (name) VALUES 
+('HTML');
+
+SELECT * FROM skills;
+
+SELECT skills.id, skills.name FROM skills
+LEFT JOIN resources_skills ON skills.id = resources_skills.skill_id
+WHERE resources_skills.skill_id IS NULL;
--- seed.sql
+++ seed.sql
@@ -1,39 +1,32 @@
 -- seed.sql remplit les tables de la base de données avec des données
 
--- INSERT INTO themes (name) VALUES
---     ('Frontend'),
---     ('Backend'),
---     ('Base de données'),
---     ('DevOps');
+INSERT INTO themes (name) VALUES
+    ('Frontend'),
+    ('Backend'),
+    ('Base de données'),
+    ('DevOps');
 
-SELECT * FROM themes;
+INSERT INTO skills (name) VALUES 
+('JavaScript'),
+('React'),
+('SQL'),
+('PostgreSQL'),
+('Node.js');
 
--- INSERT INTO skills (name) VALUES 
--- ('JavaScript'),
--- ('React'),
--- ('SQL'),
--- ('PostgreSQL'),
--- ('Node.js');
+INSERT INTO resources (title, url, description, type, is_ada, theme_id) VALUES
+('Introduction à React','https://react.dev','Guide officiel React','guide','false','1'),
+('SQL pour débutants','https://example.com/sql','Cours SQL complet','video','false','3'),
+('Exercices JavaScript','https://example.com/js','Pratique JS','exercice','false','1'),
+('Créer une API en node','https://example.com/node','Projet node','projet','false','2');
 
-SELECT * FROM skills;
-
--- INSERT INTO resources (title, url, description, type, is_ada, theme_id) VALUES
--- ('Introduction à React','https://react.dev','Guide officiel React','guide','false','1'),
--- ('SQL pour débutants','https://example.com/sql','Cours SQL complet','video','false','3'),
--- ('Exercices JavaScript','https://example.com/js','Pratique JS','exercice','false','1'),
--- ('Créer une API en node','https://example.com/node','Projet node','projet','false','2');
-
-SELECT * FROM resources;
-
-
--- INSERT INTO resources_skills (resource_id, skill_id) VALUES
--- ('1','1'),
--- ('1','2'),
--- ('2','3'),
--- ('2','4'),
--- ('3','1'),
--- ('4','1'),
--- ('4','5');
+INSERT INTO resources_skills (resource_id, skill_id) VALUES
+('1','1'),
+('1','2'),
+('2','3'),
+('2','4'),
+('3','1'),
+('4','1'),
+('4','5');
 
 SELECT * FROM resources_skills;
 
